223e6d449345f1c2dbd74dfeb18fd1fcbcfc1954
cjl-1228 <zaq5891097@gmail.com>
2022-10-13 00:59:09 +0800

    Notes WebSite

--- js/login.js
+++ js/login.js
@@ -15,3 +15,21 @@ function print(){
 
 
 }
+
+function print2(){
+    var username = document.getElementById("username2").value;
+    var password = document.getElementById("pass2").value;
+ 
+    if (username=="" || password == ""){
+        alert("帳號或密碼不得為空")
+    }
+    else if((username =="nptucbf109" || username =="NPTUCBF109")  && (password == "AAACBF109" || (password == "aaacbf109")) ){
+        alert("登入成功");
+        window.location.href = "https://cjl-1228.github.io/notes_website/web/Project/Project.html";
+    }
+    else{
+        alert("帳號或密碼錯誤");
+    }
+
+
+}
