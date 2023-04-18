72c0795fefd806ae0106a35b3a3b828d946c47c9
unknown <zaq5891097@gmail.com>
2023-04-18 10:56:58 +0800

    Notes WebSite

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ function print(){
     if (username=="" || password == ""){
         alert("帳號或密碼不得為空")
     }
-    else if(username =="zaq5891097" && password == "z5891097"){
+    else if(username =="aaa123" && password == "aaa123"){
         alert("登入成功");
         window.location.href = "https://cjl-1228.github.io/notes_website/web/Private/private.html";
     }
